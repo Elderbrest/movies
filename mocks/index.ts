2ae136c6820801b1fcba559fa62baf0ad1aebfe2
Eldar <elderblr2@gmail.com>
2022-05-07 12:23:45 +0200

    add movies mock

---
+++ mocks/index.ts
@@ -0,0 +1,35 @@
+export const categories =
+[
+  {
+    "id": "best-picture",
+    "items": [
+      {
+        "title": "Nomadland",
+        "photoUrL": "https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg",
+        "id": "nomadland"
+      },
+      {
+        "title": "The Trial of the Chicago 7",
+        "photoUrL": "https://variety.com/wp-content/uploads/2020/12/trial_of_the_chicago_seven.jpg",
+        "id": "the-trial-of-the-chicago-7"
+      },
+    ],
+    "title": "Best Picture"
+  },
+  {
+    "id": "best-director",
+    "items": [
+      {
+        "title": "Chloé Zhao for Nomadland",
+        "photoUrL": "https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg",
+        "id": "chloe-zhao"
+      },
+      {
+        "title": "Aaron Sorkin for The Trial of the Chicago 7",
+        "photoUrL": "https://variety.com/wp-content/uploads/2020/12/trial_of_the_chicago_seven.jpg",
+        "id": "aaron-sorkin"
+      },
+    ],
+    "title": "Best Director"
+  },
+];
